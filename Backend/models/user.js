9a998a8e81b54e14c19e43eb05c8f5bd310d20e7
Alex <gomezale@tamu.edu>
2024-05-27 01:38:46 -0500

    Fixed the folder structure now

---
+++ Backend/models/user.js
@@ -0,0 +1,9 @@
+const mongoose = require('mongoose');
+
+const userSchema = new mongoose.Schema({
+  username: { type: String, required: true, unique: true },
+  password: { type: String, required: true },
+  movies: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Movie' }]
+});
+
+module.exports = mongoose.model('User', userSchema);
--- movieRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const { addMovie, removeMovie } = require('../controllers/movieController');
-const router = express.Router();
-
-router.post('/add', addMovie);
-router.post('/remove', removeMovie);
-
-module.exports = router;
