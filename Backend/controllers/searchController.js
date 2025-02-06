d50d5ae494ec20e6ca364938ee744f6da917bef9
Alex <gomezale@tamu.edu>
2025-02-05 23:12:27 -0600

    Made Search Queries part of backend calls instead of keeping the API call in the client

--- Backend/controllers/searchController.js
+++ Backend/controllers/searchController.js
@@ -1,3 +1,5 @@
+// const baseLogger = require('../server');
+
 exports.searchReq = async (req,res) => {
   
     // Set the search value variable passed in from the api request
@@ -7,8 +9,6 @@ exports.searchReq = async (req,res) => {
     try { 
 
         const url = `http://www.omdbapi.com/?apikey=${apiKey}&s=${searchValue}`;
-        
-        console.log("API ENDPOINT FOR SEARCHING:", url);
         
         const response = await fetch(url);
         
@@ -28,6 +28,7 @@ exports.searchReq = async (req,res) => {
             }
         // Response is not ok (Bad (!= 200-299) Status Code)
         } else {
+            console.log("No valid response from Search API");
             res.status(500).json({ message: 'API error', error });
         }
     } catch (error) {
@@ -36,3 +37,37 @@ exports.searchReq = async (req,res) => {
     }
     
   };
+
+exports.searchAdditional = async (req,res) => {
+
+    const {movie} = req.body;
+    const apiKey = process.env.REACT_APP_OMDB_API_KEY;
+    
+    try { 
+        const url = `http://www.omdbapi.com/?apikey=${apiKey}&i=${movie.imdbID}`;
+        
+        // baseLogger.info("API ENDPOINT FOR SEARCHING IN DEPTH INFO:", url);
+        
+        const response = await fetch(url);
+        
+        // Response is ok (200-299 Status Code)
+        if (response.ok) {
+            // Create Movies List object and return to the frontend
+            const queryResponse = await response.json();
+            // If A list of movies is returned (Response == True)
+            if(queryResponse){
+                return res.status(200).json({movies: queryResponse, validResponse: true});
+            }
+            // If A list of movies is not returned (Response == False)
+            else{
+                return res.status(500).json({validResponse: false});
+            }
+        // Response is not ok (Bad (!= 200-299) Status Code)
+        } else {
+            res.status(500).json({ message: 'API error', error });
+        }
+    } catch (error) {
+        console.error('Error retrieving data from OMDb API:', error);
+        res.status(500).json({ message: 'Server error', error })
+    }
+} 
